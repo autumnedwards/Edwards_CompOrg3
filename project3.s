08928d4e4fdada55d3846f9d465a133956a0194b
Autumn Edwards <autumnedwards2@icloud.com>
2022-04-12 10:29:30 -0400

    Create project3.s
    creating an outline of the program, what must be done in each function and subfunction

---
+++ project3.s
@@ -0,0 +1,15 @@
+.data
+
+.text
+main:
+#must call Subprogram A and pass the string address into it via stack
+
+
+
+#processing the string is done in Subprogram A
+sub_a:
+#must call Subprogram B and pass the substring address via stack
+
+#processing the substring is done in Subprogram B
+sub_b:
+#decimal number or error message must be returned to Subprogram A via stack
