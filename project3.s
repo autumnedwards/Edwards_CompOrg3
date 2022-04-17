d1da77d0335c81dd8be61d9c07529526945a0e62
Autumn Edwards <autumnedwards2@icloud.com>
2022-04-17 08:22:02 -0400

    Update project3.s
    finishing touches of the skeleton of the code.  creating the jumps and sub program calls.

--- project3.s
+++ project3.s
@@ -8,13 +8,18 @@ main:
 addi $v0,8
 la $a0,userInput
 li $a1,1000
+syscall
 
+jal sub_a
 
 
 #processing the string is done in Subprogram A
 sub_a:
 #must call Subprogram B and pass the substring address via stack
 
+
 #processing the substring is done in Subprogram B
 sub_b:
 #decimal number or error message must be returned to Subprogram A via stack
+
+j $ra
