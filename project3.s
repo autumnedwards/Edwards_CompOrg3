236e99e7058841b08574a0dfcad44c4331b7b3d2
Autumn Edwards <autumnedwards2@icloud.com>
2022-04-23 11:30:19 -0400

    Update project3.s
    passing register s1 to sub_a

--- project3.s
+++ project3.s
@@ -10,18 +10,22 @@ la $a0,userInput
 li $a1,1000
 syscall
 
+#moving the userInput to $a0
+move $s1,$a0
+
 #pass data through the stack (caller)
 addi $sp, $sp, -4
 sw $fp, 0($sp)
 add $fp, $sp, $zero
-addi $sp, $sp, -12
-
+addi $sp, $sp, -4
+sw $s1, 0($sp)
 jal sub_a
 
 
 #processing the string is done in Subprogram A
 sub_a:
 #must call Subprogram B and pass the substring address via stack
+
 
 
 #processing the substring is done in Subprogram B
