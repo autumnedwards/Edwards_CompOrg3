d33730a1bd274bc5179f109d8defb8d5b63271b7
Autumn Edwards <autumnedwards2@icloud.com>
2022-04-24 10:40:00 -0400

    Update project3.s
    editing the stack that passes the address of the string to sub_a

--- project3.s
+++ project3.s
@@ -1,3 +1,10 @@
+#finding the base
+#ID= @02992633
+#02992633 % 11= 6
+#base = 26 + 6 = 32
+
+
+
 .data
 userInput: .space 1001  #saves space for the l000 characters +1 (each character is 1 byte)
 invalid: .asciiz "-"
@@ -17,9 +24,10 @@ syscall
 move $s1,$a0
 
 #pass data through the stack (caller)
-addi $sp, $sp, -4
-sw $fp, 0($sp)
-add $fp, $sp, $zero
+#frame pointer isn't needed b/c we only have one value to store but it is good practice
+#addi $sp, $sp, -4
+#sw $fp, 0($sp)
+#add $fp, $sp, $zero
 addi $sp, $sp, -4
 sw $s1, 0($sp)
 jal sub_a
@@ -30,49 +38,51 @@ syscall
 
 
 #processing the string is done in Subprogram A
-sub_a:
-#must call Subprogram B and pass the substring address via stack
-lw $t1, 0($sp)
+          sub_a:  
+            
+               move $s0, $ra #saving the return address
+               lw $t1, 0($sp) #getting my input from the stack
+               $sp, $sp, -4 #resetting the stack 
 
-#moving the address of the string from register $t1 to $a1
-move $a1, $t1
+               #moving the address of the string from register $t1 to $a1
+               move $a1, $t1
 
-#create a counter register what will count the number of characters between the delimeter
-li $t2,0
-#create a variable for the delimeter, semicolon is 59 in decimal
-li $t3, 59
-#$t4 is the counter for the total characters (should not be more than 1000)
-li $t4, 0
-#create a variable where the bytes will be stored
-li $t5, 0
+               #create a counter register what will count the number of characters between the delimeter
+              li $t2,0
+              #create a variable for the delimeter, semicolon is 59 in decimal
+              li $t3, 59
+              #$t4 is the counter for the total characters (should not be more than 1000)
+              li $t4, 0
+              #create a variable where the bytes will be stored
+              li $t5, 0
 
-parasString:
+              parasString:
 
-#lb $t5,$t4($a1)
-add $t5, $t4, $a1 
-lb $t5, ($t5)
-
-
-
-
+              #lb $t5,$t4($a1)
+              add $t5, $t4, $a1 
+              lb $t5, ($t5)
 
 
 
 
-bne $t4,1000 parasString
 
 
 
-#testing to see if the data was successfully passed to sub_a by printing the string 
-#li $v0,1
-#move $a0,$t1
-#syscall
+
+              bne $t4,1000 parasString
 
 
-#processing the substring is done in Subprogram B
-#sub_b:
-#decimal number or error message must be returned to Subprogram A via stack
 
-#j $ra
+              #testing to see if the data was successfully passed to sub_a by printing the string 
+              #li $v0,1
+              #move $a0,$t1
+              #syscall
+
+
+              #processing the substring is done in Subprogram B
+                        #sub_b:
+                            #decimal number or error message must be returned to Subprogram A via stack
+
+                            #j $ra
 
 
