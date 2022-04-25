5b0169c0983505f9c4038b261945d4655e1a963d
Autumn Edwards <autumnedwards2@icloud.com>
2022-04-25 00:03:34 -0400

    Update project3.s
    getting the base 32 value sum for a substring of two characters

--- project3.s
+++ project3.s
@@ -217,6 +217,11 @@ syscall
                                      bge $t7, 2, two #if there are two or more values in the substring continue converting 
                                      
                                      two:
+                                     mult $s4, $t3 #multiplying value by 32 
+                                     mflo $t1
+                                     add $s5, $s5, $t1
+                                     beq $t7, 2, final #if there are only two values in the substring end
+                                     bge $t7, 3, three #if there are three or more values in the substring continue converting 
                                      
                                      three:
                                      
