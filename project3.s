3e769d84ea16acc879e0ac57a3c3168c79955a48
Autumn Edwards <autumnedwards2@icloud.com>
2022-04-24 21:12:46 -0400

    Update project3.s
    changing register t7 to register s4

--- project3.s
+++ project3.s
@@ -91,9 +91,9 @@ syscall
                                       #lb $s4,$t8($t6)
                                       add $s4, $t8, $t6
                                       lb $s4, ($s4)
-                                      beq $t7, 32, increment #if the byte is a space increment 
-                                      beq $t7, 9, increment #if the byte is a tab increment 
-                                      beq $t7, 10, invalid #if there are just blank spaces and a /n then it is invalid
+                                      beq $s4, 32, increment #if the byte is a space increment 
+                                      beq $s4, 9, increment #if the byte is a tab increment 
+                                      beq $s4, 10, invalid #if there are just blank spaces and a /n then it is invalid
                                       beq $t8, 1001, invalid #if the counter reaches all 1000 characters in the blank space counter it is invalid
                                       j checkCharacterRange 
                            increment:
@@ -101,8 +101,8 @@ syscall
                                      j checkLeading
                            
                            checkCharacterRange:
-                                     blt $t7,58, possibleInt
-                                     blt $t7, 87, possibleUpper
+                                     blt $s4,58, possibleInt
+                                     blt $s4, 87, possibleUpper
                                      blt $t7, 119 possibleLower
                                      bge $t7, 119, invalid
                            
